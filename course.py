4f34f8d6029d316f43248fb5d115cbae1d97b841
Alex <alexandru_braic@yahoo.com>
2021-10-24 13:11:24 +0100

    changed main file name to course.py

---
+++ course.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy
+import os
+
+cwd = os.getcwd()
+os.chdir("G:\Alex_Learn\Projects\PythonStuff\\23-09-21")
+print("Current work:", cwd)
+
+# df1 = pandas.read_excel("supermarkets.xlsx", sheet_name=0)
+# df2 = pandas.read_csv("supermarkets-commas.txt")
+# print(df2)
